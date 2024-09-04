18345b1612d6d33d537b320aa99bf0541895b5f0
= <nelsonsamvl@gmail.com>
2024-09-04 18:20:06 +0530

    #commit

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -4,10 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <div className="
-    flex items-center font-sans justify-between bg-amber-400 px-4 py-4 font-semibold tracking-widest text-stone-800 
-    
-    ">
+    <div className="flex items-center justify-between bg-amber-400 px-4 py-4 font-sans font-semibold tracking-widest text-stone-800">
       <Link to="/">
         <h3 className="text-base uppercase sm:text-xl">Fast Pizza.co</h3>
       </Link>
