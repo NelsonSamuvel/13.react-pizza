19e686c245e7d796d49d3c20846cca2ae9299f6a
= <nelsonsamvl@gmail.com>
2024-09-04 18:17:25 +0530

    change user

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,23 +1,23 @@
-import { useDispatch, useSelector } from "react-redux";
-import Button from "../../UI/Button";
-import { deleteUser } from "./userSlice";
+import {  useSelector } from "react-redux";
+// import Button from "../../UI/Button";
+// import { deleteUser } from "./userSlice";
 
 
 function Username() {
 
   const {username} = useSelector(store => store.user);
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   if(!username) return null;
 
-  function handleDeleteUser(){
-    dispatch(deleteUser());
-    localStorage.removeItem('username');
-  }
+  // function handleDeleteUser(){
+  //   dispatch(deleteUser());
+  //   localStorage.removeItem('username');
+  // }
 
   return <div className="ml-4 flex items-center gap-4">
     <p className="text-sm hidden md:block">{username}</p>
-    <Button type="secondary" onClick={handleDeleteUser}>Logout</Button>
+    {/* <Button type="secondary" onClick={handleDeleteUser}>Logout</Button> */}
   </div>;
 }
 
